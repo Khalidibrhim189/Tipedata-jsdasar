6814af1ff555570f9c11fbaa7e66b951d1854df2
khalidjambi <khalid.jambi@gmail.comm>
2021-02-16 09:01:56 +0700

    tipedata

---
+++ js/tipedata.js
@@ -0,0 +1,12 @@
+var nama_lengkap = 'Khalid Ibrahim';
+var usia = 21;
+var sekolah = 'SMK N 3 Kota jambi';
+
+var phi= 4.13; //Bilangan pecahan
+
+// var status = true; //Boolean tipe data
+var status = false;
+
+
+console.log(status);
+
