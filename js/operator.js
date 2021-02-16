4c9772c04c112d9066b11f6b1018b382917b4682
khalidjambi <khalid.jambi@gmail.comm>
2021-02-16 09:28:57 +0700

    operator

---
+++ js/operator.js
@@ -0,0 +1,21 @@
+// Operator Matematika
+var x = 10;
+var y = 30;
+
+var penjumlahan = 10 + 30;
+
+document.getElementById('pertambahan').innerHTML = 'hasil penjumlahan 10 + 30 = ' + penjumlahan;
+
+// console.log(penjumlahan);
+
+
+// Pengurangan 
+var fulan1 = 10000;
+var fulan2 = 7500;
+
+
+var hasil_pengurangan = 10000 - 7500;
+
+document.getElementById('hasil').innerHTML = 'hasil gabungan uang fulan1 dan fulan2 adalah ' + hasil_pengurangan;
+
+console.log(hasil_pengurangan);
